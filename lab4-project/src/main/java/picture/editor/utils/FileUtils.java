57ea1470defc4e221a62f4cdc0028f34c6611d49
Aneeta-f <aneeta.f@northeastern.edu>
2021-03-26 21:35:14 -0400

    Fix issues related to check style.

--- lab4-project/src/main/java/picture/editor/utils/FileUtils.java
+++ lab4-project/src/main/java/picture/editor/utils/FileUtils.java
@@ -2,16 +2,37 @@ package picture.editor.utils;
 
 import java.io.File;
 
+/**
+ * This is an utility class responsible for performing file related operations.
+ */
 public class FileUtils {
-    public static boolean isFileExist(String filePath){
-        return new File(filePath).exists() && new File(filePath).isFile();
-    }
+  /**
+   * Method this responsible for checking either file exists or no.
+   *
+   * @param filePath
+   * @return
+   */
+  public static boolean isFileExist(String filePath) {
+    return new File(filePath).exists() && new File(filePath).isFile();
+  }
 
-    public static String getFileNameFromPath(String filePath){
-        return new File(filePath).getName();
-    }
+  /**
+   * Return file name from path.
+   *
+   * @param filePath
+   * @return
+   */
+  public static String getFileNameFromPath(String filePath) {
+    return new File(filePath).getName();
+  }
 
-    public static String getDirFromPath(String filePath){
-        return new File(filePath).getParent();
-    }
+  /**
+   * Return directory from path.
+   *
+   * @param filePath
+   * @return
+   */
+  public static String getDirFromPath(String filePath) {
+    return new File(filePath).getParent();
+  }
 }
